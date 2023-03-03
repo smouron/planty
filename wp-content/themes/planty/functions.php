8ee996b64ba6fab3debadc5f096c78e71ed52a13
Stephane Mouron <stephane.mouron@free.fr>
2023-03-03 12:05:22 +0100

    Nettoyage des différents fichiers de surcharge de styles et quelques ajustements pour le Header

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -46,8 +46,8 @@ function form_order_func($atts) {
             <form class="quantity">
                 <div class="relative">
                     <input class="order-quantity" type="number" value="0" min="0" max="100" />
-                    <input type="text" class="btn-more btn-quantity" value="+" />
-                    <input type="text" class="btn-less btn-quantity" value="-" />
+                    <span class="btn-more btn-quantity">+</span> 
+                    <span class="btn-less btn-quantity">-</span> 
                 </div>
                 <input type="submit" class="btn-ok" value="Ok" />
             </form>
@@ -61,8 +61,8 @@ function form_order_func($atts) {
             <form class="quantity">
                 <div class="relative">
                     <input class="order-quantity" type="number" value="0" min="0" max="100" />
-                    <input type="text" class="btn-more btn-quantity" value="+" />
-                    <input type="text" class="btn-less btn-quantity" value="-" />
+                    <span class="btn-more btn-quantity">+</span> 
+                    <span class="btn-less btn-quantity">-</span> 
                 </div>
                 <input type="submit" class="btn-ok" value="Ok" />
             </form>
