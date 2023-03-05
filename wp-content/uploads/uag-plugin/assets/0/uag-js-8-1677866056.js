7442786bd7b49df9aa77f5d0c70c0e7e10c0cadd
Stephane Mouron <stephane.mouron@free.fr>
2023-03-05 22:46:37 +0100

    Essais de resynchronisation du repositoie

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-8-1677866056.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"091337b2","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"planty.drinks@gmail.com, stephane.mouron@free.fr","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-091337b2', 8 );
+});
+ })
